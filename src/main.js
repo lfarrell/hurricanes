71471939604657b76be53be5831e12fe1f8e30f7
lfarrell <farrelldlfarrell@gmail.com>
2017-10-23 20:10:29 -0400

    Fix tooltip for IE

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import 'babel-polyfill'
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
